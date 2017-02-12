f45ca3034d8cf69307c0b2dcc60a913d5c82f018
aashaysood <aashaysood@outlook.com>
2017-02-12 16:55:24 +0000

    New Commit

--- src/com/jpm/test/Assessment.java
+++ src/com/jpm/test/Assessment.java
@@ -6,6 +6,7 @@ package com.jpm.test;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Comparator;
@@ -46,72 +47,89 @@ public class Assessment {
 			setInstruction();
 			getInstruction();
 			
-			//check if Settlement Date needs to be moved
-			if(currency.equals("AED")||currency.equals("SAR")){
-				setlDate = getSetlDate((String)instructionList.get(5));
-				dow = dateUtility((String)instructionList.get(5));
-				if(dow==DayOfWeek.FRIDAY){
-					cal.setTime(setlDate);
-					cal.add(Calendar.DATE, 2);
-					setlDate = cal.getTime();
-				}else if(dow==DayOfWeek.SATURDAY){
-					cal.setTime(setlDate);
-					cal.add(Calendar.DATE, 1);
-					setlDate = cal.getTime();
+			try{
+				//check if Settlement Date needs to be moved
+				if(currency.equals("AED")||currency.equals("SAR")){
+					setlDate = getSetlDate((String)instructionList.get(5));
+					dow = dateUtility((String)instructionList.get(5));
+					if(dow==DayOfWeek.FRIDAY){
+						cal.setTime(setlDate);
+						cal.add(Calendar.DATE, 2);
+						setlDate = cal.getTime();
+					}else if(dow==DayOfWeek.SATURDAY){
+						cal.setTime(setlDate);
+						cal.add(Calendar.DATE, 1);
+						setlDate = cal.getTime();
+					}
+				}else{
+					setlDate = getSetlDate((String)instructionList.get(5));
+					dow = dateUtility((String)instructionList.get(5));
+					if(dow==DayOfWeek.SATURDAY){
+						cal.setTime(setlDate);
+						cal.add(Calendar.DATE, 2);
+						setlDate = cal.getTime();
+					}else if(dow==DayOfWeek.SUNDAY){
+						cal.setTime(setlDate);
+						cal.add(Calendar.DATE, 1);
+						setlDate = cal.getTime();
+					}
 				}
-			}else{
-				setlDate = getSetlDate((String)instructionList.get(5));
-				dow = dateUtility((String)instructionList.get(5));
-				if(dow==DayOfWeek.SATURDAY){
-					cal.setTime(setlDate);
-					cal.add(Calendar.DATE, 2);
-					setlDate = cal.getTime();
-				}else if(dow==DayOfWeek.SUNDAY){
-					cal.setTime(setlDate);
-					cal.add(Calendar.DATE, 1);
-					setlDate = cal.getTime();
+				
+				//Set trade amount
+				double tradeAmount=0.0;
+				if(BS.equals("B")){
+					tradeAmount = getTradeAmt(agreedFx, units, unitPrice);
+					totalOutAmount=totalOutAmount+tradeAmount;
+					outgoingList.add(new Entity((String)instructionList.get(0), tradeAmount));
+				}else if(BS.equals("S")){
+					tradeAmount = getTradeAmt(agreedFx, units, unitPrice);
+					totalInAmount=totalInAmount+tradeAmount;
+					incomingList.add(new Entity((String)instructionList.get(0), tradeAmount));
 				}
-			}
-			
-			//Set trade amount
-			double tradeAmount=0.0;
-			if(BS.equals("B")){
-				tradeAmount = getTradeAmt(agreedFx, units, unitPrice);
-				totalOutAmount=totalOutAmount+tradeAmount;
-				outgoingList.add(new Entity((String)instructionList.get(0), tradeAmount));
-			}else if(BS.equals("S")){
-				tradeAmount = getTradeAmt(agreedFx, units, unitPrice);
-				totalInAmount=totalInAmount+tradeAmount;
-				incomingList.add(new Entity((String)instructionList.get(0), tradeAmount));
+				
+			}catch(NullPointerException e){
+				System.out.println("Null pointer exception occured.");
+			}catch(Exception e){
+				System.out.println("Exception occured in for Loop.");
 			}
 		}
 		
 		System.out.println("The total Incoming Amount is: " + totalInAmount);
 		System.out.println("The total Outgoing Amount is: " + totalOutAmount);
-
-		// Incoming List Sort
-		System.out.println("Incoming Sorted List");
-		System.out.println("Entity Name		Entity Amount");
-		incomingList.sort(Comparator.comparing(Entity::getAmount));
-		for(int i=incomingList.size()-1;i>=0;i--){
-			System.out.println(((Entity)incomingList.get(i)).getName()+"			"+((Entity)incomingList.get(i)).getAmount());
-		}
 		
-		// Outgoing List Sort
-		System.out.println("Outgoing Sorted List");
-		System.out.println("Entity Name		Entity Amount");
-		outgoingList.sort(Comparator.comparing(Entity::getAmount));
-		for(int i=outgoingList.size()-1;i>=0;i--){
-			System.out.println(((Entity)outgoingList.get(i)).getName()+"			"+((Entity)outgoingList.get(i)).getAmount());
+		try{
+			
+			// Incoming List Sort
+			System.out.println("Incoming Sorted List");
+			System.out.println("Entity Name		Entity Amount");
+			incomingList.sort(Comparator.comparing(Entity::getAmount));
+			for(int i=incomingList.size()-1;i>=0;i--){
+				System.out.println(((Entity)incomingList.get(i)).getName()+"			"+((Entity)incomingList.get(i)).getAmount());
+			}
+			
+			// Outgoing List Sort
+			System.out.println("Outgoing Sorted List");
+			System.out.println("Entity Name		Entity Amount");
+			outgoingList.sort(Comparator.comparing(Entity::getAmount));
+			for(int i=outgoingList.size()-1;i>=0;i--){
+				System.out.println(((Entity)outgoingList.get(i)).getName()+"			"+((Entity)outgoingList.get(i)).getAmount());
+			}
+		}catch(NullPointerException e){
+			System.out.println("Null pointer exception at comparator.");
 		}
 		
 	}
 	
 	public static Date getSetlDate(String setlDate){
 		
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMM yyyy", Locale.ENGLISH);
-		LocalDate date = LocalDate.parse(setlDate, formatter);
-		return java.sql.Date.valueOf(date);
+		try{
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMM yyyy", Locale.ENGLISH);
+			LocalDate date = LocalDate.parse(setlDate, formatter);
+			return java.sql.Date.valueOf(date);
+		}catch(NullPointerException e){
+			System.out.println("Exception occurred in getSetlDate.");
+		}
+		return null;
 	}
 	
 	public static double getTradeAmt(double agreedFx, long units, double unitPrice){
@@ -123,20 +141,29 @@ public class Assessment {
 	
 	public static DayOfWeek dateUtility(String setlDate){
 		
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMM yyyy", Locale.ENGLISH);
-		LocalDate date = LocalDate.parse(setlDate, formatter);
-		DayOfWeek dow = date.getDayOfWeek();
-		return dow;
+		try{
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMM yyyy", Locale.ENGLISH);
+			LocalDate date = LocalDate.parse(setlDate, formatter);
+			DayOfWeek dow = date.getDayOfWeek();
+			return dow;
+		}catch(DateTimeParseException e){
+			System.out.println("DateTime cannot be parsed.");
+		}
+		return null;
 	}
 	
 	public static void getInstruction(){
 		
-		entity = (String)instructionList.get(0);
-		BS = (String)instructionList.get(1);
-		agreedFx = (double)instructionList.get(2);
-		currency = (String)instructionList.get(3);
-		units = (int)instructionList.get(6);
-		unitPrice = (double)instructionList.get(7);
+		try{
+			entity = (String)instructionList.get(0);
+			BS = (String)instructionList.get(1);
+			agreedFx = (double)instructionList.get(2);
+			currency = (String)instructionList.get(3);
+			units = (int)instructionList.get(6);
+			unitPrice = (double)instructionList.get(7);
+		}catch(ClassCastException e){
+			System.out.println("Exception in getInstruction.");
+		}
 		
 	}
 	public static void setInstruction(){
